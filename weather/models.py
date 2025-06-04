f18ab76845d2dffc5787337b9c594520cf62bb60
DylanUr <147861299+DylanUr@users.noreply.github.com>
2025-06-04 16:56:37 -0700

    Backend listo de login, register, historial y endpoints
    login y register tienen el formato predeterminado de django, falta mejorar frontend

--- weather/models.py
+++ weather/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 #Historial de busquedas de los usuarios
 class SearchHistory(models.Model):
-    city = models.CharField(max_length=100)
-    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    
+    temperature = models.FloatField()
+    humidity = models.IntegerField()
+    description = models.CharField(max_length=100)
+    icon = models.CharField(max_length=10)
+
+    searched_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.city or f'{self.latitude},{self.longitude}'} at {self.searched_at.strftime('%Y-%m-%d %H:%M:%S')}"
